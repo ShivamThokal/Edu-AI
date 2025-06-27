b4151db1947725ce795c1cdecd2a540040a2e564
Amol Rathod <129649335+amolrr@users.noreply.github.com>
2025-06-27 18:40:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const cors = require("cors");
 // const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const fileUpload = require('express-fileupload');
 
 
 const OpenAI = require("openai");
@@ -19,9 +19,19 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB error:", err));
 
+//cloudinary connection
+const cloudinaryConnection = require('./Config/Cloudinary');
+cloudinaryConnection();
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({
+  tempFileDir:'/tmp',
+  useTempFiles: true,
+}))
+
 
 // console.log("Loaded API key:", OPENAI_API_KEY);
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
